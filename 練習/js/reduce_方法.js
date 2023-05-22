36a114c09ab1cc5f9c62d10824e5df131b25b1c7
Hhank9137 <hank042698168@gmail.com>
2023-05-22 15:33:53 +0800

    0522/PHP( 連接MySQL )

--- 練習/js/reduce_方法.js
+++ 練習/js/reduce_方法.js
@@ -1,1 +1,32 @@
-// reduce()方法 讓陣列加總
+
+// reduce()方法 讓陣列做運算(全部)
+var nn = [1,2,3,5,6,7,8,10,11,13,14,15];
+var nsum = nn.reduce(function(sum,x){
+    sum += x;
+    return sum;
+},0)
+console.log(nsum)
+
+// reduce()方法 for迴圈版本(運算)
+
+var x = 0;
+for (var i = 0; i < nn.length ; i++){
+
+    x += nn[i];
+    console.log(x);
+}
+
+// ES6 (const、let)
+// var 作用域在 function
+// let、const 作用域在 {}
+
+
+// let 
+// const
+
+
+var i = 0;
+for (let i =0; i<10; i++){
+    i += i;
+}
+console.log(i)
